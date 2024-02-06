dfb52d6fea12f62b64e3bfb87d5eb6b9b96fed57
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-02-06 19:46:53 +0900

    주문과 할인 도메인 개발

--- src/main/java/hello/core/member/MemberRepository.java
+++ src/main/java/hello/core/member/MemberRepository.java
@@ -1,2 +1,6 @@
-package hello.core.member;public interface MemberRepository {
+package hello.core.member;
+
+public interface MemberRepository {
+    void save(Member member);
+    Member findById(Long member);
 }
--- src/main/java/hello/core/member/MemberService.java
+++ src/main/java/hello/core/member/MemberService.java
@@ -1,2 +1,9 @@
-package hello.core.member;public interface MemberService {
+package hello.core.member;
+
+public interface MemberService {
+
+    void join(Member member);
+
+    Member findMember(Long memberId);
+
 }
