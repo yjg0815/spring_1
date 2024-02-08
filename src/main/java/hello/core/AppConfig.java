5a92488954357ee43de844b97fd142c2b22d4f5d
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-02-08 17:48:31 +0900

    스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,25 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 인자를 바로 new 로 넘기지 말고, 그 new 한 클래스의 역할이 드러나게 각각 분리
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
-
+    
+    // 스프링으로 전환 => 각 메소드들을 스프링 빈으로 등록
+    // 스프링 컨테이너를 사용하면 어떤 이점이 있길래...?
+    // ==> 엄청 많다고.....
 
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); //스프링 컨테이너 생성
+        //스프링 컨테이너에 AppConfig를 등록, 객체 생성 및 연결
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 찾을 것의 이름(class 이름, 타입)
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
