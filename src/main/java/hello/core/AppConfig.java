0897872e3383834ce4b652e115e778291161ab2a
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-02-08 17:15:48 +0900

    새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,2 +1,33 @@
-package hello.core;public class AppConfig {
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    // 인자를 바로 new 로 넘기지 말고, 그 new 한 클래스의 역할이 드러나게 각각 분리
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,2 +1,38 @@
-package hello.core.order;public class OrderServiceImpl {
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //할인 정책을 변경하려면 이 코드를 고쳐야 함 ==> OCP 위반 (클라이언트 코드를 고쳐야 함)
+    //orderservieimple이 실제 구체 클래스에서도 의존하고 있음 ==> DIP 위반
+    
+    // 이 문제를 어떻게 해결할까 => DIP를 위반하지 않도록 인터페이스만 의존하도록 설계
+    
+    private final DiscountPolicy discountPolicy;
+    // 근데 이렇게 하면 nullpointexception이 생김 => 당연함 실제 구체가 없으니까
+    // 어떻게 해결? 누군가 구체를 대신 생성해서 집어넣어줘야함
+    // ==> AppConfig : 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
 }
