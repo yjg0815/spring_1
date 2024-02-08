b7adab2df5c999a05170a766c4b5d6cbe74c9044
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-02-08 18:23:33 +0900

    컨테이너에 등록된 모든 빈 조회

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -1,2 +1,41 @@
-package hello.core.beanfind;public class ApplicationContextInfoTest {
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) { // iter + enter : 꺼내는 자동 for문
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + "object = " + bean); //sout + enter : 자동 print문
+        }
+    }
+
+    @Test  // ctrl + d : 코드 그대로 복사해서 밑에 넣어줌
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) { // iter + enter : 꺼내는 자동 for문
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // 스프링 내부 정보 제외 내가 넣은것만 꺼내게 해줌
+            // Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE : 스프링 내부에서 사용하는 빈
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + "object = " + bean); //sout + enter : 자동 print문
+            }
+        }
+    }
+
 }
