e2cee04fd6f639ad3a50ef0ebea58274e2c4215e
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-02-11 21:43:21 +0900

    필터

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        // 어디서부터 하위로 내려들어가서 찾을건지 지정 가능, 여러개 지정 가능
+        // 지정 안하면 얘가 붙은 패키지 부터 그 하위에 있는 것들을 뒤짐
+        //웬만하면 프로젝트 최상단에 설정정보가 있는 appconfig를 두자.
+
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         // 수동으로 등록한 AppConfig는 자동 등록한 대상에서 뺴주기 위해 필터 설정
         // 보통 이렇게 안하는데, 이건 앞에서 한 예제 코드들을 유지하기 위해서 그냥 필터 달아줌
