f2f5d3f702ce522a596385837775fee57ed52f8e
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-02-11 17:36:22 +0900

    @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -34,7 +34,23 @@ public class ConfigurationSingletonTest {
 
         /*
         AppConfig에서 new로 여러번 호출한 것 같은데, 출력해보면 모두 같은 인스턴스이다.
+        @Configuration 에서 해준다/
          */
+    }
 
+    @Test
+    void configurationDepp() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+
+        /*
+        순수한 클래스라면 class hello.core.AppConfig 까지만 출력되어야 함.
+        but 출력 값 : bean = class hello.core.AppConfig$$SpringCGLIB$$0
+        ==> AppConfig를 상속 받은 다른 클래스를 등록 한 것 (바이트 코드를 조작 해서..)
+        이걸 @Configuration에서 해주는 것.
+        @Bean만 사용해도 스프링 빈으로 등록해주지만, 이것만으로는 싱글톤 보장 x
+         */
     }
 }
