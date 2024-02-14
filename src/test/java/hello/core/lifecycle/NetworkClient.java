f0924f7cc71a25d4afacf6b83f58aad2ef6af51c
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-02-14 20:33:49 +0900

    인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.web.servlet.ServletContextInitializerBeans;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -25,5 +27,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    // 스프링 전용 인터페이스에 의존, 내가 코드를 고칠 수 없는 외부 라이브러리에 적용 불가
+    // => 스프링 초창기에 나온 방법이라 지금은 거의 사용 x
+    @Override // 의존관계 주입이 끝나면 호출
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
